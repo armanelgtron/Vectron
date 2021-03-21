41eb95c938296d9144ebeeb8420995397173e426
Armanelgtron <armanelgtron@gmail.com>
2021-03-21 03:42:24 -0400

    change config handling

--- js/config.js
+++ js/config.js
@@ -28,39 +28,98 @@ var config_scrollWheelZoom = true;
 var config_snapToPosition = true;
 var config_autoAdjustGridSpacing = true;
 
-function config_load()
+// default values:
+function _config_check_default(item)
 {
-    // Initialize default values if necessary
-    if(localStorage.getItem("darkTheme") == null)
-        localStorage.setItem("darkTheme","true");
-    
-    if(localStorage.getItem("showInfoBar") == null)
-        localStorage.setItem("showInfoBar","false"); 
-    
-    if(localStorage.getItem("showDebug") == null)
-        localStorage.setItem("showDebug","false");
-    
-    
-    // Load saved values
-    if(localStorage.getItem("darkTheme") == "true")
-        enable_dark_theme();
-    
-    if(localStorage.getItem("showInfoBar") == "true")
-        show_info_bar();
-    
-    if(localStorage.getItem("showDebug") == "false")
-        hide_debug();
+    switch(item)
+    {
+        case "darkTheme": return "true";
+        case "showInfoBar": return "false";
+        case "showDebug": return "false";
+    }
 }
 
 
-function enable_dark_theme()
+
+function _config_get(item)
+{
+    if(window.localStorage)
+    {
+        return localStorage.getItem(item);
+    }
+    else if(window.sessionStorage)
+    {
+        return sessionStorage.getItem(item);
+    }
+    else
+    {
+        if(!window._localStorage) window._localStorage = {};
+        if(window._localStorage[item] === undefined) return null;
+        else return window._localStorage[item];
+    }
+}
+
+function _config_set(item,value)
+{
+    if(window.localStorage)
+    {
+        return localStorage.setItem(item,value);
+    }
+    else if(window.sessionStorage)
+    {
+        return sessionStorage.setItem(item,value);
+    }
+    else
+    {
+        if(!window._localStorage) window._localStorage = {};
+        window._localStorage[item] = value;
+    }
+}
+
+function _config_check(item)
+{
+    var value = _config_get(item);
+    if(value === null)
+        value = _config_check_default(item);
+
+    return (value=="true");
+}
+
+function _config_set_enable(item)
+{
+    return _config_set(item,"true");
+}
+function _config_set_disable(item)
+{
+    return _config_set(item,"false");
+}
+
+
+
+function config_load()
+{
+    // load values without changing anything
+    if(_config_check("darkTheme"))
+        enable_dark_theme(true);
+    
+    if(_config_check("showInfoBar"))
+        show_info_bar(true);
+    
+    if(!_config_check("showDebug"))
+        hide_debug(true);
+}
+
+
+var __darktheme_has_loaded = false;
+function enable_dark_theme(noset)
 {
     var theme = document.getElementById("theme");
-    if("onload" in theme)
+    if("onload" in theme && !__darktheme_has_loaded)
     {
         theme.onload = function()
         {
             config_isDark = true;
+            __darktheme_has_loaded = true;
             vectron_render();
         };
     }
@@ -72,9 +131,9 @@ function enable_dark_theme()
     theme.href = "./css/vectron-dark.css";
     
     document.getElementById("dark-theme").checked = true;
-    localStorage.setItem("darkTheme","true");
+    if(!noset) _config_set_enable("darkTheme");
 }
-function disable_dark_theme()
+function disable_dark_theme(noset)
 {
     var theme = document.getElementById("theme");
     if(theme.onload) theme.onload = null;
@@ -84,10 +143,10 @@ function disable_dark_theme()
     vectron_render();
     
     document.getElementById("dark-theme").checked = false;
-    localStorage.setItem("darkTheme","false");
+    if(!noset) _config_set_disable("darkTheme");
 }
 
-function show_info_bar()
+function show_info_bar(noset)
 {
     document.getElementsByClassName("info")[0].style.display = "";
     //marginRight
@@ -95,29 +154,29 @@ function show_info_bar()
     vectron_render();
     
     document.getElementById("show-info-bar").checked = true;
-    localStorage.setItem("showInfoBar","true");
+    if(!noset) _config_set_enable("showInfoBar");
 }
-function hide_info_bar()
+function hide_info_bar(noset)
 {
     document.getElementsByClassName("info")[0].style.display = "none";
     document.getElementById("canvas_container").style.right = "";
     vectron_render();
     
     document.getElementById("show-info-bar").checked = false;
-    localStorage.setItem("showInfoBar","false");
+    if(!noset) _config_set_disable("showInfoBar");
 }
 
-function show_debug()
+function show_debug(noset)
 {
     document.getElementById("debug_box").className = "";
     
     document.getElementById("show-debug-panel").checked = true;
-    localStorage.setItem("showDebug","true");
+    if(!noset) _config_set_enable("showDebug");
 }
-function hide_debug()
+function hide_debug(noset)
 {
     document.getElementById("debug_box").className = "nodbug";
     
     document.getElementById("show-debug-panel").checked = false;
-    localStorage.setItem("showDebug","false");
+    if(!noset) _config_set_disable("showDebug");
 }
