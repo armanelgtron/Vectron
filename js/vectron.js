dfd9c2ce711bcab736caaa87d84e4e3614d38501
Armanelgtron <armanelgtron@gmail.com>
2021-03-30 22:17:33 -0400

    bump version to 1.1.1

--- js/vectron.js
+++ js/vectron.js
@@ -23,7 +23,7 @@ You should have received a copy of the GNU General Public License
 along with Vectron.  If not, see <http://www.gnu.org/licenses/>.
 
 */
-window.vtVersion = 1.102;
+window.vtVersion = 1.110;
 
 var vectron_width;
 var vectron_height;
